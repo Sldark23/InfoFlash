6a06384580f9524b8a0042487fb72beab770ba5d
Sldark23 <115877460+Sldark23@users.noreply.github.com>
2025-02-12 00:52:08 -0300

    update script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('notícias/')
+    .then(response => response.text())
+    .then(data => {
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(data, 'text/html');
+        let links = doc.querySelectorAll('a');
+
+        let newsContainer = document.getElementById('news-container');
+        links.forEach(link => {
+            let fileName = link.getAttribute('href');
+            if (fileName.endsWith('.html')) {
+                let title = fileName.replace('.html', '').replace(/-/g, ' ');
+                let newsItem = `
+                    <div class="news-item">
+                        <div class="news-title">${title}</div>
+                        <a class="read-more" href="notícias/${fileName}">Ler mais</a>
+                    </div>
+                `;
+                newsContainer.innerHTML += newsItem;
+            }
+        });
+    })
+    .catch(error => console.log('Erro ao carregar as notícias:', error));
+});
